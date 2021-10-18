52011211cf02088ded8fe89fec3f4a25149004c5
Manoj Fernando <mj.bitwave@gmail.com>
2021-10-19 00:07:35 +0530

    Section02 code

--- handler.js
+++ handler.js
@@ -1,14 +1,22 @@
 "use strict";
 
-module.exports.hello = async (event) => {
+const fib = (n) => {
+  if (n <= 1) return n;
+  return fib(n - 1) + fib(n - 2);
+};
+
+module.exports.generate = async (event) => {
+  // Generate fibonacci number for the given number
+  let result = 0;
+  const number = parseInt(event.pathParameters.number);
+  if (number) {
+    result = fib(number);
+  }
+
+  // throw new Error("Some error occured");
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Hello World!"
-      },
-      null,
-      2
-    )
+    body: JSON.stringify({ result })
   };
 };
